b861689945cef0f42e35e35233f479db8872c1e5
Doan Phuc <mrjared16>
2019-07-19 23:37:30 +0700

    rewrite some functions

--- script/dom-scrapper.js
+++ script/dom-scrapper.js
@@ -8,93 +8,133 @@ function DOMScrapper(response) {
 
 DOMScrapper.prototype.addListenerToDOM = function () {
     // add sound onclick listener
-    Array.from(this.getSoundButtons()).forEach(node =>
+    Array.from(this.scrapeSoundButtons()).forEach(node =>
         node.addEventListener("click", OnclickFunction.playSound, false));
 
     // add collocation onclick listener
-    Array.from(this.getCollapseButtons()).forEach(node =>
+    Array.from(this.scrapeCollapseButtons()).forEach(node =>
         node.addEventListener("click", OnclickFunction.collapseToggle, false));
 }
 
 DOMScrapper.prototype.getResponseForTooltip = function () {
-    const main_content = this.getMainContent();
+    const main_content = this.scrapeMainContent();
     if (!main_content)
-        return;
+        return Content.outDate();
 
     const word_info = Array.from(main_content.childNodes);
-    const pronun = Array.from(this.getPronunciation());
-
-    while (pronun && pronun.length > 0 && pronun[pronun.length - 1].parentNode != pronun[0].parentNode) {
-        pronun.pop();
-    }
-
     if (this.isResponseError(word_info)) {
         return Content.outDate();
     }
 
-    const defs = Array.from(this.getOtherDefinition());
-    let def_object = null;
-    if (defs.length > 0) {
-        def_object = this.getOtherDefinitonObject(defs);
-    }
+    const word = this.getWord(word_info);
+    const content = this.getContent(word_info);
+    const pronunciation = this.getPronunciation();
+    const type = this.getWordType();
+    const example = this.getExamples();
 
-    return Content.reconstructTooltip([word_info[0]], word_info.slice(1), pronun, def_object);
+    return Content.reconstructTooltip(word, content, pronunciation, type, example);
 }
 
 DOMScrapper.prototype.isResponseError = function (word_info) {
-    return (!word_info || word_info.length < 2 || !this.isWordContainer(word_info[0]));
-}
-
-DOMScrapper.prototype.getSoundButtons = function () {
-    return this.html.getElementsByClassName("audio_play_button");
-}
-
-DOMScrapper.prototype.getCollapseButtons = function () {
-    return this.html.querySelectorAll(".unbox .heading");
-}
-
-DOMScrapper.prototype.getMainContent = function () {
-    return this.html.querySelector("div.entry > .h-g");
-}
-
-DOMScrapper.prototype.getPronunciation = function () {
-    return this.html.querySelectorAll(".top-g>.pron-gs, .top-g>.collapse");
+    return (!word_info || !word_info.length || word_info.length < 2 || !this.isWordContainer(word_info[0]));
 }
 
 DOMScrapper.prototype.isWordContainer = function (node) {
     return node.classList.contains("top-container");
 }
 
-DOMScrapper.prototype.getOtherDefinition = function () {
-    return this.html.querySelectorAll('.arl1');
-
+DOMScrapper.prototype.scrapeSoundButtons = function () {
+    return this.html.getElementsByClassName("audio_play_button");
 }
 
-DOMScrapper.prototype.getOtherDefinitonObject = function (defs) {
-    let getAnchor = () => {
-        return this.html.querySelector(".webtop-g");
-    }
-    const _anchor = getAnchor();
+DOMScrapper.prototype.scrapeCollapseButtons = function () {
+    return this.html.querySelectorAll(".unbox .heading");
+}
 
+DOMScrapper.prototype.scrapeMainContent = function () {
+    return this.html.querySelector("div.entry > .h-g");
+}
 
-    let getClass = (def) => {
-        return def.getElementsByTagName('pos')[0].innerText;
+DOMScrapper.prototype.getWord = function (word) {
+    return [word[0]];
+}
+
+DOMScrapper.prototype.getContent = function (word) {
+    return word.slice(1);
+}
+
+DOMScrapper.prototype.getPronunciation = function () {
+    const scrapePronunciation = () => this.html.querySelectorAll(".top-g>.pron-gs, .top-g>.collapse");
+    const pronunciation = Array.from(scrapePronunciation());
+    if (!pronunciation.hasOwnProperty('length'))
+        return null;
+
+    while (pronunciation.length > 0 && pronunciation[pronunciation.length - 1].parentNode != pronunciation[0].parentNode) {
+        pronunciation.pop();
     }
-    let getWordPath = (def) => {
-        let paths = (def.parentNode.href).split('/');
+
+    return this.getObjectTemplate(pronunciation, pronunciation[0].parentNode, pronunciation[0].previousSibling);
+}
+
+DOMScrapper.prototype.getWordType = function () {
+    const scrapeWordType = () => this.html.querySelectorAll('.arl1');
+
+    let types = Array.from(scrapeWordType());
+    if (!types.hasOwnProperty('length') || !types.length)
+        return null;
+
+    // word type
+    const getClass = (type) => {
+        return type.getElementsByTagName('pos')[0].innerText;
+    }
+
+    // path of url
+    const getPath = (type) => {
+        let paths = (type.parentNode.href).split('/');
         return paths[paths.length - 1];
     }
-    let templateObject = (clss, pth) => {
-        return {
-            class: clss,
-            path: pth,
-            anchor: _anchor
-        }
+
+    const getParent = () => {
+        return this.html.querySelector(".webtop-g");
     }
 
-    let result = [];
-    defs.forEach(def => {
-        result.push(templateObject(getClass(def), getWordPath(def)));
+    types = types.map(type => ({
+        class: getClass(type),
+        path: getPath(type)
+    }));
+    return this.getObjectTemplate(types, getParent());
+}
+
+DOMScrapper.prototype.getExamples = function () {
+    const scrapeExample = () => this.html.querySelectorAll('.def');
+    const getParent = element => element[0].parentNode;
+    const getAnchor = element => element[0].previousSibling;
+    const getExample = element => {
+        const example = [];
+        while (element.nextElementSibling) {
+            element = element.nextElementSibling;
+            example.push(element);
+        }
+        return example;
+    }
+
+    const result = [];
+    const definitions = scrapeExample();
+    // each definition may be have example
+    definitions.forEach(def => {
+        const example = getExample(def);
+        if (example.length === 0) {
+            return;
+        }
+        const parent = getParent(example);
+        const anchor = getAnchor(example);
+        result.push(this.getObjectTemplate(example, parent, anchor));
     });
     return result;
 }
+
+DOMScrapper.prototype.getObjectTemplate = (element, parent, anchor) => ({
+    element,
+    parent,
+    anchor: (anchor) ? anchor : null
+});
